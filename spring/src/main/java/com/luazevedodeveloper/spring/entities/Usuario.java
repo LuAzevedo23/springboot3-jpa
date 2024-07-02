8c9cb2b920b74fe3ecf2da84c1bb7bd1497b85cf
Luciene Azevedo <luciene.azevedo@hotmail.com>
2024-07-01 21:35:31 -0300

    Jason com sucesso BD H2

--- spring/src/main/java/com/luazevedodeveloper/spring/entities/Usuario.java
+++ spring/src/main/java/com/luazevedodeveloper/spring/entities/Usuario.java
@@ -1,17 +1,22 @@
 package com.luazevedodeveloper.spring.entities;
 
-import lombok.Getter;
-import lombok.Setter;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 import java.io.Serial;
 import java.io.Serializable;
 
-@Setter
-@Getter
+@Data
+@Entity
 public class Usuario implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
